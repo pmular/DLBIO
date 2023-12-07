0c577f9e8c0161570dc31e51f6c7dfa017ea9b3a
KenjiTet <kenji.tetard@epfl.ch>
2023-12-07 11:27:31 +0100

    added attention layer to transform

--- methods/transform.py
+++ methods/transform.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import numpy as np 
+import torch.nn.functional as F
 
 
 # metrics
@@ -67,3 +68,42 @@ class SelfOT():
         return embedding
 
 
+class SelfAttention(nn.Module):
+    def __init__(self):
+        super(SelfAttention, self).__init__()
+        self.query_layer = None
+        self.key_layer = None
+        self.value_layer = None
+
+    def forward(self, x):
+        # Dynamically create the layers if not already created
+        if self.query_layer is None or self.key_layer is None or self.value_layer is None:
+            dim = x.size(-1)
+            self.query_layer = nn.Linear(dim, dim).to(x.device)
+            self.key_layer = nn.Linear(dim, dim).to(x.device)
+            self.value_layer = nn.Linear(dim, dim).to(x.device)
+
+        # Compute query, key, and value matrices
+        queries = self.query_layer(x)
+        keys = self.key_layer(x)
+        values = self.value_layer(x)  # Transform inputs into 'values'
+
+        # Compute attention scores
+        scores = torch.matmul(queries, keys.transpose(-1, -2)) 
+
+        # Apply softmax to get attention weights
+        attention_probs = F.softmax(scores, dim=-1)
+
+
+
+        return attention_probs
+
+"""
+dummy_input = torch.randn(4, 10)  # batch_size=4, seq_len=10, feature_dim=20
+
+# Initialize the SelfAttention layer and test it with the dummy input
+self_attention = SelfAttention()
+attended_features = self_attention(dummy_input)
+
+# Print the output dimensions and the output for verification
+print("Attended features dimensions:", attended_features.shape)"""
