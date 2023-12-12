4d13892932237426fec68f68fbabc48ffab8f57b
pmular <pau.mulet.arabi@gmail.com>
2023-12-12 11:40:49 +0000

    new model

--- methods/protonet_sot.py
+++ methods/protonet_sot.py
@@ -29,37 +29,21 @@ class ProtoNetSOT(ProtoNet):
             z_all = self.feature.forward(x) # embedding with backbone
             z_all = self.transform(z_all)
             z_all = z_all.view(self.n_way, self.n_support + self.n_query, -1)
-        z_support = z_all[:, :self.n_support]
-        z_query = z_all[:, self.n_support:]
 
-        return z_support, z_query
+        return z_all
 
-
-class ProtoNet(MetaTemplate):
-    def __init__(self, backbone, n_way, n_support):
-        super(ProtoNet, self).__init__(backbone, n_way, n_support)
-        self.loss_fn = nn.CrossEntropyLoss()
-
+    
     def set_forward(self, x, is_feature=False):
         
-        z_support, z_query = self.parse_feature(x, is_feature)
-        
-        z_support = z_support.contiguous()
-        z_proto = z_support.view(self.n_way, self.n_support, -1).mean(1)  # centroids
-        z_query = z_query.contiguous().view(self.n_way * self.n_query, -1)
+        z_all = self.parse_feature(x, is_feature)
+        n_tot = self.n_query+self.n_support
+        mass = torch.stack([torch.sum(z_all[...,i*n_tot:i*n_tot+self.n_support], dim=2) for i in range(self.n_way)]).permute(1, 2, 0)
+        scores = mass[:, self.n_support:, :].flatten(0, 1)
 
-        dists = euclidean_dist(z_query, z_proto)
-        scores = -dists
         return scores
 
 
-    def set_forward_loss(self, x):
-        y_query = torch.from_numpy(np.repeat(range( self.n_way ), self.n_query ))
-        y_query = Variable(y_query.cuda())
-
-        scores = self.set_forward(x)
-
-        return self.loss_fn(scores, y_query )
+ 
 
 
 def euclidean_dist( x, y):
