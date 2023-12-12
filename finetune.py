eb8b355e449b7d93b23d0560fb5e10412b5a795b
KenjiTet <kenji.tetard@epfl.ch>
2023-12-12 15:00:04 +0100

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -1,46 +1,63 @@
 import subprocess
 import wandb
 
-def run_experiment(config):
+# This function will be called by wandb for each set of hyperparameters
+def train():
+    # Retrieve hyperparameters from wandb
+    run = wandb.init(reinit=True)  # This initializes the run and allows reinitialization
+    config = run.config
+
+    # Convert the list of layer dimensions to a Hydra-friendly format
+    layer_dim_str = ','.join(map(str, config['layer_dim']))
+
     # Construct the command to run `run.py` with the given config
     command = [
         "python3", "run.py",
-        f"exp.name= test_finetune",
-        f"method=protonet_sot",
-        f"dataset=swissprot",
-        f"self_ot.reg={config['reg']}",
-        f"self_ot.diag_weight={config['diag_weight']}",
-        f"self_ot.tol={config['tol']}",
-        f"self_ot.tau={config['tau']}",
+        "exp.name=test_finetune",
+        "method=protonet_sot",
+        "dataset=swissprot",
+        f"method.cls.self_ot.reg={config['reg']}",
         f"optimizer_cls.lr={config['lr']}",
-        f"self_ot.tol={config['tol']}"
-
+        f"backbone.layer_dim=[{layer_dim_str}]",
+        f"backbone.dropout={config['dropout']}",
     ]
 
     # Run the command
     subprocess.run(command)
+    run.finish()  # This ends the current run
+
+
 
 def main():
-    # Define a set of hyperparameters to test
-    hyperparameters = [
-        {'reg': 0.05, 'diag_weight': 50, 'tol': 1e-5, 'tau': 10, 'lr':0.001},
-        {'reg': 0.1, 'diag_weight': 100, 'tol': 1e-6, 'tau': 100, 'lr':0.001},
-        # Add more configurations as needed
-    ]
+    # Define the sweep configuration
+    sweep_config = {
+        'method': 'bayes',  # or 'random' or 'bayes'
+        'metric': {
+            'name': 'accuracy',
+            'goal': 'maximize'
+        },
+        'parameters': {
+            'reg': {
+                'values': [0.05, 0.1, 0.15]
+            },
+            'lr': {
+                'values': [0.005, 0.001, 0.0005]
+            },
+            'layer_dim': {
+                'values': [[64,64], [256], [128], [128, 128]]
+            },
+            'dropout': {
+                'values': [0.0, 0.1, 0.3]
+            },
+        }
+    }
 
-    # Initialize a wandb project
-    wandb.init(project='your_project_name', entity='your_wandb_entity')
+    wandb.login()
+    sweep_id = wandb.sweep(sweep_config, project='DL for Bio', entity='kenjitetard0')
 
-    # Iterate over the hyperparameters and run experiments
-    for config in hyperparameters:
-        # Log the current configuration to wandb
-        wandb.config.update(config)
+    # Run the sweep
+    wandb.agent(sweep_id, function=train)
 
-        # Run the experiment with the current configuration
-        run_experiment(config)
-
-    # Finish the wandb session
-    wandb.finish()
 
 if __name__ == "__main__":
     main()
